d1afe953bb97680ca99145dd8e30a0f45b85b885
January <january147@outlook.com>
2019-11-06 17:54:33 +0800

    small improvements in start.sh

--- start.sh
+++ start.sh
@@ -12,19 +12,23 @@ do
 done
 
 ######################config###################
-image="../image/ubuntu-12.04.5-server-i386.iso"
 hd="../image/qemu_compat.img"
-option="-monitor stdio -m 1024 -usbdevice tablet -netdev user,id=mynet,hostfwd=tcp::5022-:22 -device rtl8139,netdev=mynet"
-
 hd2="../image/xp.qcow2"
+info_display="-monitor stdio"
+mem="-m 1024"
+mouse="-usbdevice tablet"
+network="-netdev user,id=mynet,hostfwd=tcp::5022-:22 -device rtl8139,netdev=mynet"
+kvm="-enable-kvm"
+
 # window7的安装镜像
 cdrom="../image/windows7_32.iso"
-
+# ubuntu安装镜像
+image="../image/ubuntu-12.04.5-server-i386.iso"
 # decaf++的qemu
 machine1="../decaf/i386-softmmu/qemu-system-i386"
 # 我电脑上安装的qemu
 machine2=qemu-system-i386
-
+# 默认启动DECAF++虚拟机
 if [ -z "$choose" ] || [ $choose == "?" ];then
     choose=1
 fi
@@ -32,9 +36,12 @@ fi
 
 # run
 if [ "$choose" = "1" ];then
+    # DECAF++用的qemu比较老，需要安装VNCviewer来显示界面
     vncviewer 127.0.0.1:5900
+    option="$info_display $mem $mouse $network"
     $machine1 $option -hda $hd2
-# 启用kvm加速 
 else
-    $machine2 $option -hda $hd2 -enable-kvm 
+    # 启用kvm加速 
+    option="$info_display $mem $mouse $network $kvm"
+    $machine2 $option -hda $hd2
 fi
