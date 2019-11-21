6fc39b70f475941e992779e72cf0db886c44b925
January <january147@outlook.com>
2019-11-21 19:29:22 +0800

    improvement of plugin

---
+++ plugins/proc_monitor/config-plugin.h
@@ -0,0 +1,6 @@
+#define DEFINE_INSN_BEGIN
+#define DEFINE_INSN_END
+#define DEFINE_BLOCK_BEGIN
+#define DEFINE_BLOCK_END
+#define DECAF_HOME "/home/january/workplace/DECAF-1.91/decaf"
+#define PLUGIN_PATH "/home/january/workplace/DECAF-1.91/DECAF-test/plugins/proc_monitor"
---
+++ plugins/proc_monitor/proc_monitor.d
@@ -0,0 +1,123 @@
+proc_monitor.o: proc_monitor.c \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_types.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-common.h \
+ /home/january/workplace/DECAF-1.91/decaf/compiler.h \
+ /home/january/workplace/DECAF-1.91/decaf/config-host.h \
+ /usr/include/glib-2.0/glib.h /usr/include/glib-2.0/glib/galloca.h \
+ /usr/include/glib-2.0/glib/gtypes.h \
+ /usr/lib/x86_64-linux-gnu/glib-2.0/include/glibconfig.h \
+ /usr/include/glib-2.0/glib/gmacros.h \
+ /usr/include/glib-2.0/glib/gversionmacros.h \
+ /usr/include/glib-2.0/glib/garray.h \
+ /usr/include/glib-2.0/glib/gasyncqueue.h \
+ /usr/include/glib-2.0/glib/gthread.h \
+ /usr/include/glib-2.0/glib/gatomic.h /usr/include/glib-2.0/glib/gerror.h \
+ /usr/include/glib-2.0/glib/gquark.h /usr/include/glib-2.0/glib/gutils.h \
+ /usr/include/glib-2.0/glib/gbacktrace.h \
+ /usr/include/glib-2.0/glib/gbase64.h \
+ /usr/include/glib-2.0/glib/gbitlock.h \
+ /usr/include/glib-2.0/glib/gbookmarkfile.h \
+ /usr/include/glib-2.0/glib/gbytes.h \
+ /usr/include/glib-2.0/glib/gcharset.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/gconvert.h \
+ /usr/include/glib-2.0/glib/gdataset.h /usr/include/glib-2.0/glib/gdate.h \
+ /usr/include/glib-2.0/glib/gdatetime.h \
+ /usr/include/glib-2.0/glib/gtimezone.h /usr/include/glib-2.0/glib/gdir.h \
+ /usr/include/glib-2.0/glib/genviron.h \
+ /usr/include/glib-2.0/glib/gfileutils.h \
+ /usr/include/glib-2.0/glib/ggettext.h /usr/include/glib-2.0/glib/ghash.h \
+ /usr/include/glib-2.0/glib/glist.h /usr/include/glib-2.0/glib/gmem.h \
+ /usr/include/glib-2.0/glib/gnode.h /usr/include/glib-2.0/glib/ghmac.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/ghook.h \
+ /usr/include/glib-2.0/glib/ghostutils.h \
+ /usr/include/glib-2.0/glib/giochannel.h \
+ /usr/include/glib-2.0/glib/gmain.h /usr/include/glib-2.0/glib/gpoll.h \
+ /usr/include/glib-2.0/glib/gslist.h /usr/include/glib-2.0/glib/gstring.h \
+ /usr/include/glib-2.0/glib/gunicode.h \
+ /usr/include/glib-2.0/glib/gkeyfile.h \
+ /usr/include/glib-2.0/glib/gmappedfile.h \
+ /usr/include/glib-2.0/glib/gmarkup.h \
+ /usr/include/glib-2.0/glib/gmessages.h \
+ /usr/include/glib-2.0/glib/gvariant.h \
+ /usr/include/glib-2.0/glib/gvarianttype.h \
+ /usr/include/glib-2.0/glib/goption.h \
+ /usr/include/glib-2.0/glib/gpattern.h \
+ /usr/include/glib-2.0/glib/gprimes.h /usr/include/glib-2.0/glib/gqsort.h \
+ /usr/include/glib-2.0/glib/gqueue.h /usr/include/glib-2.0/glib/grand.h \
+ /usr/include/glib-2.0/glib/gregex.h \
+ /usr/include/glib-2.0/glib/gscanner.h \
+ /usr/include/glib-2.0/glib/gsequence.h \
+ /usr/include/glib-2.0/glib/gshell.h /usr/include/glib-2.0/glib/gslice.h \
+ /usr/include/glib-2.0/glib/gspawn.h \
+ /usr/include/glib-2.0/glib/gstrfuncs.h \
+ /usr/include/glib-2.0/glib/gstringchunk.h \
+ /usr/include/glib-2.0/glib/gtestutils.h \
+ /usr/include/glib-2.0/glib/gthreadpool.h \
+ /usr/include/glib-2.0/glib/gtimer.h \
+ /usr/include/glib-2.0/glib/gtrashstack.h \
+ /usr/include/glib-2.0/glib/gtree.h \
+ /usr/include/glib-2.0/glib/gurifuncs.h \
+ /usr/include/glib-2.0/glib/guuid.h /usr/include/glib-2.0/glib/gversion.h \
+ /usr/include/glib-2.0/glib/deprecated/gallocator.h \
+ /usr/include/glib-2.0/glib/deprecated/gcache.h \
+ /usr/include/glib-2.0/glib/deprecated/gcompletion.h \
+ /usr/include/glib-2.0/glib/deprecated/gmain.h \
+ /usr/include/glib-2.0/glib/deprecated/grel.h \
+ /usr/include/glib-2.0/glib/deprecated/gthread.h \
+ /usr/include/glib-2.0/glib/glib-autocleanups.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-os-posix.h \
+ /home/january/workplace/DECAF-1.91/decaf/target-i386/cpu.h \
+ /home/january/workplace/DECAF-1.91/decaf/config.h \
+ /home/january/workplace/DECAF-1.91/decaf/i386-softmmu/config-target.h \
+ /home/january/workplace/DECAF-1.91/decaf/cpu-defs.h \
+ /home/january/workplace/DECAF-1.91/decaf/config.h \
+ /home/january/workplace/DECAF-1.91/decaf/osdep.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-queue.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-barrier.h \
+ /home/january/workplace/DECAF-1.91/decaf/targphys.h \
+ /home/january/workplace/DECAF-1.91/decaf/fpu/softfloat.h \
+ /home/january/workplace/DECAF-1.91/decaf/config-host.h \
+ /home/january/workplace/DECAF-1.91/decaf/cpu-all.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-common.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-tls.h \
+ /home/january/workplace/DECAF-1.91/decaf/cpu-common.h \
+ /home/january/workplace/DECAF-1.91/decaf/bswap.h \
+ /home/january/workplace/DECAF-1.91/decaf/target-i386/svm.h \
+ /home/january/workplace/DECAF-1.91/decaf/hw/apic.h \
+ /home/january/workplace/DECAF-1.91/decaf/exec-all.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-log.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-lock.h \
+ /home/january/workplace/DECAF-1.91/decaf/softmmu_defs.h \
+ /home/january/workplace/DECAF-1.91/decaf/softmmu_header.h \
+ /home/january/workplace/DECAF-1.91/decaf/module.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_main.h \
+ /home/january/workplace/DECAF-1.91/decaf/monitor.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-char.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-option.h \
+ /home/january/workplace/DECAF-1.91/decaf/qdict.h \
+ /home/january/workplace/DECAF-1.91/decaf/qobject.h \
+ /home/january/workplace/DECAF-1.91/decaf/qlist.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-config.h \
+ /home/january/workplace/DECAF-1.91/decaf/qstring.h \
+ /home/january/workplace/DECAF-1.91/decaf/main-loop.h \
+ /home/january/workplace/DECAF-1.91/decaf/qerror.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-error.h \
+ /home/january/workplace/DECAF-1.91/decaf/error.h \
+ /home/january/workplace/DECAF-1.91/decaf/block.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-aio.h \
+ /home/january/workplace/DECAF-1.91/decaf/qemu-coroutine.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_types.h \
+ /home/january/workplace/DECAF-1.91/decaf/blockdev.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_callback.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_callback_common.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_types.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_callback_common.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/vmi_callback.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/utils/Output.h \
+ /home/january/workplace/DECAF-1.91/decaf/target-i386/DECAF_target.h \
+ /home/january/workplace/DECAF-1.91/decaf/target-i386/cpu.h \
+ /home/january/workplace/DECAF-1.91/decaf/targphys.h \
+ /home/january/workplace/DECAF-1.91/decaf/compiler.h \
+ /home/january/workplace/DECAF-1.91/decaf/shared/DECAF_callback.h
